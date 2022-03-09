22ac79625faded7fd066bc5c5a0a4e522d6be84c
ESLL3N <esllenescolar@gmail.com>
2022-03-09 17:44:18 +0000

    operador ternário

---
+++ Operadores.java
@@ -0,0 +1,47 @@
+/*public class Operadores {
+    public static void main(String[] args){
+        int num = 0;                                                                      //<- operador ternário
+
+        String msg = (num > 10) ? "Número maior que 10" : "Número menor que 10";
+        System.out.println(msg);
+    }
+}*/
+
+/*public class Operadores {
+    public static void main(String[] args){
+        int num = 50;
+
+        String msg =((num > 10) && (num < 100)) ?
+        "Número maior que 10 e ou menor que 100" :
+        "Número menor ou  igual à 10 ou maior que 100";
+        System.out.println(msg);
+    }
+}
+*/
+
+/*ou pode usar com if
+public class Operadores {
+    public static void main(String[] args){
+        int num = 50;
+    if ((num > 10) && (num < 100)) {
+        msg = "Número maior que 10 e ou menor que 100";
+    } else {
+        msg = "Número menor ou  igual à 10 ou maior que 100";
+    }
+    System.out.println(msg);
+}
+*/
+
+// exercício para ver se um número é par ou impar
+// para saber se um número é par ou impar podemos utilizar a expressão (num % 2 == 0), já que se um número é par seu resto sempre sera 0.
+
+/*public class Operadores {
+    public static void main(String[] args){
+        int num = 57;
+
+        String msg = (num % 2 == 0) ? "O número é par" : "O número é impar";
+        System.out.println(msg);
+    }
+}
+
+*/
